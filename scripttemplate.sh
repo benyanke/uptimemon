5000571707453bf51ec3aa2cc63bcdbb29620a35
Ben Yanke <ben@benyanke.com>
2017-03-08 12:37:43 -0600

    added template

---
+++ scripttemplate.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+##############################################
+# uptimemod - simple uptime monitor
+#
+# Setup script for uptimemod, a monitoring
+# script for web services
+#
+# Created by Ben Yanke
+# https://github.com/benyanke/uptimemond
+##############################################
+
+
+
